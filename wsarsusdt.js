d7a661c6277cda324b9cd2a7cdfcc25791c5f794
MartindeOrta <82926690+MartindeOrta@users.noreply.github.com>
2023-10-17 11:28:20 -0300

    Update wsarsusdt.js

--- wsarsusdt.js
+++ wsarsusdt.js
@@ -9,7 +9,7 @@ let stockObject = JSON.parse(event.data);
 console.log(stockObject);
 
 let price=parseFloat(stockObject.p).toFixed(2);
-stockPriceElement.innerText = "Precio usdt: $"+ price + "AR";
+stockPriceElement.innerText = "Precio usdt: AR$"+ price;
 stockPriceElement.style.color = !lastPrice || lastPrice === price ? 'black' : lastPrice < price ? 'green' : 'red';
 lastPrice= price;
 
